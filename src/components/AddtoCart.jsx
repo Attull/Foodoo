3c81fc556859fd69f986d4a4c57962c9a6b29f69
Attull <atul852798@gmail.com>
2024-09-30 21:33:27 +0530

    total quantity and total amount

--- src/components/AddtoCart.jsx
+++ src/components/AddtoCart.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import ItemsCart from './ItemsCart'
+import { useSelector } from 'react-redux'
 
 const AddtoCart = (props) => {
+
+  const cartItems = useSelector((state)=> state.cart.cart)
+  const totalQuantity =  cartItems.reduce((acc,item)=>{
+    return item.qty + acc
+  },0)
+
+  const totalPrice =  cartItems.reduce((acc,item)=>{
+    return item.qty* item.price + acc
+  },0)
 
   const {setShowCart} = props
   return (
@@ -14,8 +24,8 @@ const AddtoCart = (props) => {
             <ItemsCart />
         </div>
         <div class="cart-footer">
-            <h3>Items : 2</h3>
-            <h3>Total Amout : 320</h3>
+            <h3>Items : {totalQuantity}</h3>
+            <h3>Total Amout : {totalPrice}</h3>
             <hr/>
             <button className='checkout-btn'>Checkout</button>
         </div>
